4e94d397c46c371c59fa6962d034d376d237969e
Suriya-aero-360 <suriyaprakash.a@aero360.co.in>
2025-07-30 18:23:43 +0530

    models

--- time_management/api_urls.py
+++ time_management/api_urls.py
@@ -20,8 +20,9 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+
 # from time_management.models import (
 #     User,
 #     Employee,
@@ -547,4 +548,4 @@ urlpatterns = [
     path("company-policy/<int:id>/", company_policy_api),
     # path("api/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     # path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
