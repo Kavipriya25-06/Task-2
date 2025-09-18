fa39063838b66fc39b84dd4fc3cf94caf35cc433
Suriya-aero-360 <suriyaprakash.a@aero360.co.in>
2025-09-18 15:17:44 +0530

    static

--- my_project/settings.py
+++ my_project/settings.py
@@ -180,7 +180,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/app2/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
